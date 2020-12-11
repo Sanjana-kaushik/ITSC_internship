ef088840c83be6778d2ffa4255c1ca6ab9469210
Sanjana-kaushik <70919195+Sanjana-kaushik@users.noreply.github.com>
2020-12-11 15:33:50 -0500

    Final changes to the webpage

--- src/pages/DevOps.jsx
+++ src/pages/DevOps.jsx
@@ -10,57 +10,53 @@ import logophoto from '../assets/images/logodevops.jpg';
 const Dev = () => {
   const { devops } = useRouteData();
   return (
-  <div className="default-content">
-    <div className="content">
-      <div>
-        <ReactPlayer
-          url="https://youtu.be/DSGyEsJ17cI"
-        />
+    <div className="default-content">
+      <div className="content">
+
+        <h1>DevOps project</h1>
       </div>
-      <h1>DevOps project</h1>
-    </div>
-    <Fragment>
-      <Row>
-        <Col xl="4" lg="6">
-          <Card className="mb-5">
-            <img alt="..." className="card-img-top" src={logophoto} />
-            <CardBody>
-              <h5 className="card-title font-weight-bold font-size-lg">
-                Project Display
-              </h5>
-              <p className="card-text">
-                Description of the project
-              </p>
+      <Fragment>
+        <Row>
+          <Col xl="4" lg="6">
+            <Card className="mb-5">
+              <img alt="..." className="card-img-top" src={logophoto} />
+              <CardBody>
+                <h5 className="card-title font-weight-bold font-size-lg">
+                  Project Display
+                </h5>
+                <p className="card-text">
+                  Description of the project
+                </p>
                 {devops.map(dev =>
                   <li key={dev.id}>
                     <Link to={`/DevOps/dev/${dev.id}/`}>{dev.title}</Link>
                   </li>)}
-            </CardBody>
-          </Card>
-        </Col>
-        <Col xl="4" lg="6">
-          <Card className="mb-5">
-            <img alt="..." className="card-img-top" src={student1} />
-            <CardBody>
-              <h5 className="card-title font-weight-bold font-size-lg">
-                Project creator
-              </h5>
-              <p className="card-text">
-                Bio of the project creator
-              </p>
-              <Button
-                tag="a"
-                color="primary"
-                href="#/"
-                onClick={e => e.preventDefault()}>
-                Go somewhere
-              </Button>
-            </CardBody>
-          </Card>
-        </Col>
-        <Col xl="4" className="d-none d-xl-block" />
-      </Row>
-    </Fragment>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xl="4" lg="6">
+            <Card className="mb-5">
+              <img alt="..." className="card-img-top" src={student1} />
+              <CardBody>
+                <h5 className="card-title font-weight-bold font-size-lg">
+                  Project creator
+                </h5>
+                <p className="card-text">
+                  Bio of the project creator
+                </p>
+                <Button
+                  tag="a"
+                  color="primary"
+                  href="#/"
+                  onClick={e => e.preventDefault()}>
+                  Go somewhere
+                </Button>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xl="4" className="d-none d-xl-block" />
+        </Row>
+      </Fragment>
       <div>
         <ReactPlayer
           url="https://youtu.be/DSGyEsJ17cI"
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,29 +1,72 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactPlayer from 'react-player';
-import FancyDiv from '../components/FancyDiv';
-import Schedule from '../assets/images/Schedule.jpg';
+import { Table } from 'reactstrap';
+import logo from '../assets/images/ITExpo.jpg';
 
 const IndexPage = () =>
-  <div className="default-content">
-    <div className="content">
+  <Fragment>
+    <div className="default-content">
+      <div className="content">
+
+        <div style={{ textAlign: `center` }}>
+          <img src={logo} alt='IT Expo' />
+          <h2>UC School of Information Technology</h2>
+        </div>
+
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-9">
+              <h1>Schedule </h1>
+
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    <div className="table-responsive-md">
+      <Table hover bordered striped className="mb-5">
+        <thead className="thead-light">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Time</th>
+            <th scope="col">Name</th>
+            <th scope="col">Designation</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>9:05 AM - 10:00 AM</td>
+            <td>Kristi Nelson</td>
+            <td>Vice President and Provost - University of Cincinnati</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>10:05 AM - 11:00 AM</td>
+            <td>David Adams</td>
+            <td>CIO - University of Cincinnati</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>11:05 AM - 12:00 PM</td>
+            <td>Hazem Said</td>
+            <td>Director and Professor - School of IT</td>
+          </tr>
+          <tr>
+            <th scope="row">4</th>
+            <td>12:05 pM - 1:00 PM</td>
+            <td>Lawrence Johnson</td>
+            <td>Dean and Professor - CECH</td>
+          </tr>
+        </tbody>
+      </Table>
       <div>
         <ReactPlayer
-          url="https://youtu.be/JhHycQeCMTk"
+          url="https://youtu.be/ofDJLWwmr_A"
         />
       </div>
-      <FancyDiv>
-        <div style={{ textAlign: `center` }}>
-          <h1>
-            Welcome to React-Static <br />
-          </h1>
-          <p>
-            Start Here
-          </p>
-        </div>
-      </FancyDiv>
-      <div class="container">
-            </div>
     </div>
-  </div>;
 
+  </Fragment>;
 export default IndexPage;
